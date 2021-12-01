be1b62d59d727d5aa9710d5509d399274f1a7ad7
almustaphamuha98@gmail.com <almustaphamuha98@gmail.com>
2021-12-01 20:52:47 +0100

    Added celery config to config.py

--- config.py
+++ config.py
@@ -6,3 +6,5 @@ class Config:
     DEBUG = True if os.environ.get("FLASK_ENV", "development") == "development" else False
     MAX_CONTENT_LENGTH = 16 * 1024 * 1024
     PORT = os.environ.get("PORT", 5000) 
+    CELERY_BROKER_URL = os.environ.get("CELERY_BROKER_URL", "redis://localhost:6379/0")
+    CELERY_RESULT_BACKEND = os.environ.get("CELERY_RESULT_BACKEND", "redis://localhost:6379/1")
