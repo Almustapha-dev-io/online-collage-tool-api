20a8eacbbf8dd5b6d54735cf12a91197c66f0397
almustaphamuha98@gmail.com <almustaphamuha98@gmail.com>
2021-12-01 19:16:30 +0100

    Added parametes to config.py

--- config.py
+++ config.py
@@ -1,5 +1,8 @@
 import os
 
 class Config:
+    SECRET_KEY = os.environ.get("SECRET_KEY", "asupersecretsecret")
+    ENV = os.environ.get("FLASK_ENV", "development")
+    DEBUG = True if os.environ.get("FLASK_ENV", "development") == "development" else False
     MAX_CONTENT_LENGTH = 16 * 1024 * 1024
     PORT = os.environ.get("PORT", 5000) 
